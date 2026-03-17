8995a3c2d843e45056bec7ecd2e355f99b70ef3d
Nathaniel <nathanielmujesia@gmail.com>
2026-03-17 10:23:47 +0300

    style:add slider layout and style

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
-const primaryHeader = document.querySelector('#primary-header');
-const primaryNav = document.querySelector('#primary-navigation');
-const mobileNavToggle = document.querySelector('#mobile-nav-toggle');
+const primaryHeader = document.querySelector("#primary-header");
+const primaryNav = document.querySelector("#primary-navigation");
+const mobileNavToggle = document.querySelector("#mobile-nav-toggle");
 
-mobileNavToggle.addEventListener('click', () => {
-    primaryNav.hasAttribute('data-visible') ? primaryNav.setAttribute('aria-expanded', false) : primaryNav.setAttribute('aria-expanded', true);
-    primaryNav.toggleAttribute('data-visible');
-    primaryHeader.toggleAttribute('data-overlay');
-})
+mobileNavToggle.addEventListener("click", () => {
+    primaryNav.hasAttribute("data-visible")
+        ? primaryNav.setAttribute("aria-expanded", false)
+        : primaryNav.setAttribute("aria-expanded", true);
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
+});
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+    slidesToShow: 1,
+    arrows: false, // arrows enabled 767px and down
+    dots: true,
+    centerMode: true,
+    responsive: {
+        480: {            
+            arrows: false,
+            slidesToShow: 2
+        },
+        880: {        
+            slidesToShow: 3,     
+            dots: false,
+        },
+        1280: {
+            slidesToShow: 4,
+            
+        },
+    },
+});
